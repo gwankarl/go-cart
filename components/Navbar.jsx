0891fba20e832393ec23a93d3c0d574a6b372987
gwankarl <gwankarl@gmail.com>
2026-07-10 20:18:07 +0100

    added stripe checkout payment

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -29,9 +29,11 @@ const Navbar = () => {
           >
             <span className='text-green-600'>go</span>cart
             <span className='text-green-600 text-5xl leading-0'>.</span>
-            <p className='absolute text-xs font-semibold -top-1 -right-8 px-3 p-0.5 rounded-full flex items-center gap-2 text-white bg-green-500'>
-              plus
-            </p>
+            <Protect plan='plus'>
+              <p className='absolute text-xs font-semibold -top-1 -right-8 px-3 p-0.5 rounded-full flex items-center gap-2 text-white bg-green-500'>
+                plus
+              </p>
+            </Protect>
           </Link>
 
           {/* Desktop Menu */}
